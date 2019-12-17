f6ea7d3128299c5968005748698e6ed210912407
Changyu Liu <liuchangyu1111@gmail.com>
2019-12-17 12:16:48 +0800

    Update gan_mnist.py

--- gan_mnist.py
+++ gan_mnist.py
@@ -22,11 +22,10 @@ import torch.utils.data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
-import torchsummary
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataroot', type=str,
-                    default="./datasets/mnist", help='path to datasets.')
+                    default="./datasets", help='path to datasets.')
 parser.add_argument('--workers', type=int,
                     help='number of data loading workers', default=2)
 parser.add_argument('--batch_size', type=int,
@@ -43,7 +42,7 @@ parser.add_argument('--nz', type=int, default=100,
                     help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--epochs', type=int, default=800, help="Train loop")
+parser.add_argument('--epochs', type=int, default=500, help="Train loop")
 parser.add_argument('--ngpu', type=int, default=1,
                     help='number of GPUs to use')
 parser.add_argument('--netG', default='',
@@ -112,7 +111,7 @@ class Generator(nn.Module):
       *block(128, 256),
       *block(256, 512),
       *block(512, 1024),
-      nn.Linear(1024, 784 * 3),
+      nn.Linear(1024, 784),
       nn.Tanh()
     )
 
@@ -128,7 +127,7 @@ class Generator(nn.Module):
         self.main, inputs, range(self.ngpu))
     else:
       outputs = self.main(inputs)
-    return outputs.view(outputs.size(0), *(3, 28, 28))
+    return outputs.view(outputs.size(0), *(1, 28, 28))
 
 
 class Discriminator(nn.Module):
@@ -140,7 +139,7 @@ class Discriminator(nn.Module):
     self.ngpu = ngpu
 
     self.main = nn.Sequential(
-      nn.Linear(784 * 3, 512),
+      nn.Linear(784, 512),
       nn.LeakyReLU(0.2, inplace=True),
       nn.Linear(512, 256),
       nn.LeakyReLU(0.2, inplace=True),
@@ -167,14 +166,15 @@ class Discriminator(nn.Module):
 def train():
   """ train model
   """
-  dataset = dset.ImageFolder(root=opt.dataroot,
-                             transform=transforms.Compose([
-                               transforms.ToTensor(),
-                               transforms.Normalize(
-                                 (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                             ]))
+  dataset = dset.MNIST(root=opt.dataroot,
+                       download=True,
+                       train=True,
+                       transform=transforms.Compose([
+                         transforms.ToTensor(),
+                         transforms.Normalize(
+                           (0.5,), (0.5,)),
+                       ]))
 
-  assert dataset
   dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size,
                                            shuffle=True, num_workers=int(opt.workers))
 
@@ -245,10 +245,10 @@ def train():
 
         errG.backward()
         optimizerG.step()
-      print(f"Epoch->[{epoch + 1:3d}/{opt.epochs}] "
-            f"Progress->[{i}/{len(dataloader)}] "
-            f"Loss_D: {errD.item():.4f} "
-            f"Loss_G: {errG.item():.4f} ", end="\r")
+        print(f"Epoch->[{epoch + 1:3d}/{opt.epochs}] "
+              f"Progress->[{i}/{len(dataloader)}] "
+              f"Loss_D: {errD.item():.4f} "
+              f"Loss_G: {errG.item():.4f} ", end="\r")
 
       if i % 5 == 0:
         vutils.save_image(
