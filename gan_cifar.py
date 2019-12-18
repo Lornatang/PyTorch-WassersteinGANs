0575e9a493d62ad7ad93cbcc2f9052380f149791
Changyu Liu <liuchangyu1111@gmail.com>
2019-12-18 19:43:58 +0800

    Update gan_cifar.py

--- gan_cifar.py
+++ gan_cifar.py
@@ -97,24 +97,41 @@ def weights_init(m):
 class Generator(nn.Module):
   def __init__(self, ngpu):
     super(Generator, self).__init__()
+    self.channels = 3
     self.ngpu = ngpu
+
+    def block(in_channels, out_channels, kernel_size, stride, padding,
+              normalize=True):
+      """ simple layer struct.
+      Args:
+        in_channels (int): Number of channels in the input image
+        out_channels (int): Number of channels produced by the convolution
+        kernel_size (int or tuple): Size of the convolving kernel
+        stride (int or tuple, optional): Stride of the convolution.
+        padding (int or tuple, optional): ``dilation * (kernel_size - 1) - padding`` zero-padding
+            will be added to both sides of each dimension in the input.
+        normalize (bool, optional): If ``True``, accelerating Deep Network Training
+          by Reducing Internal Covariate Shift`. Default: ``True``
+
+      Examples:
+        >>> print(*block(100, ngf * 8, 4, 1, 0))
+
+      Returns:
+        A list layer.
+      """
+      layers = [nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride,
+                                   padding, bias=False)]
+      if normalize:
+        layers.append(nn.BatchNorm2d(out_channels))
+      layers.append(nn.ReLU(inplace=True))
+      return layers
+
     self.main = nn.Sequential(
-      # inputs is Z, going into a convolution
-      nn.ConvTranspose2d(100, ngf * 4, 4, 1, 0, bias=False),
-      nn.BatchNorm2d(ngf * 4),
-      nn.ReLU(True),
-      # state size. (ngf*4) x 4 x 4
-      nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ngf * 2),
-      nn.ReLU(True),
-      # state size. (ngf*2) x 8 x 8
-      nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ngf),
-      nn.ReLU(True),
-      # state size. (ngf) x 16 x 16
-      nn.ConvTranspose2d(ngf, 3, 4, 2, 1, bias=False),
+      *block(100, ngf * 4, 4, 1, 0),
+      *block(ngf * 4, ngf * 2, 4, 2, 1),
+      *block(ngf * 2, ngf, 4, 2, 1),
+      nn.ConvTranspose2d(ngf, self.channels, 4, 2, 1),
       nn.Tanh()
-      # state size. (nc) x 32 x 32
     )
 
   def forward(self, inputs):
@@ -134,21 +151,45 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
   def __init__(self, ngpu):
     super(Discriminator, self).__init__()
+    self.channels = 3
     self.ngpu = ngpu
+
+    def block(in_channels, out_channels, kernel_size, stride, padding,
+              normalize=True):
+      """ simple layer struct.
+      Args:
+        in_channels (int): Number of channels in the input image
+        out_channels (int): Number of channels produced by the convolution
+        kernel_size (int or tuple): Size of the convolving kernel
+        stride (int or tuple, optional): Stride of the convolution.
+        padding (int or tuple, optional): Zero-padding added to both sides of the input.
+        normalize (bool, optional): If ``True``, accelerating Deep Network Training
+          by Reducing Internal Covariate Shift`. Default: ``True``
+
+      Examples:
+        >>> print(*block(self.channels, ndf, 4, 2, 1))
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride,
+                      padding, dilation, groups,
+                      bias, padding_mode)]
+            layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+
+      Returns:
+        A list layer.
+      """
+      layers = [nn.Conv2d(in_channels, out_channels, kernel_size, stride,
+                          padding, bias=False)]
+      if normalize:
+        layers.append(nn.BatchNorm2d(out_channels))
+      layers.append(nn.LeakyReLU(0.2, inplace=True))
+      return layers
+
+    # create network sequential.
     self.main = nn.Sequential(
-      # inputs is (nc) x 32 x 32
-      nn.Conv2d(3, ndf, 4, 2, 1, bias=False),
-      nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf) x 16 x 16
-      nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 2),
-      nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*2) x 8 x 8
-      nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 4),
-      nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*4) x 4 x 4
-      nn.Conv2d(ndf * 4, ndf * 8, 4, 1, 0, bias=False),
+      *block(self.channels, ndf, 4, 2, 1, normalize=False),
+      *block(ndf, ndf * 2, 4, 2, 1),
+      *block(ndf * 2, ndf * 4, 4, 2, 1),
+      nn.Conv2d(ndf * 4, 1, 4, 1, 0),
       nn.Sigmoid()
     )
 
@@ -160,11 +201,11 @@ class Discriminator(nn.Module):
       forwarded data.
     """
     if torch.cuda.is_available() and self.ngpu > 1:
-      outputs = nn.parallel.data_parallel(
-        self.main, inputs, range(self.ngpu))
+      outputs = nn.parallel.data_parallel(self.main, inputs, range(self.ngpu))
     else:
       outputs = self.main(inputs)
     return outputs.view(-1, 1).squeeze(1)
+
 
 
 def train():
@@ -273,7 +314,7 @@ def generate():
   ################################################
   print(f"Load model...\n")
   if torch.cuda.device_count() > 1:
-    netG = torch.nn.DataParallel(Generator(opt.ngpu)).to(device)
+    netG = torch.nn.DataParallel(Generator(opt.ngpu))
   else:
     netG = Generator(opt.ngpu)
   netG.load_state_dict(torch.load(opt.netG, map_location=lambda storage, loc: storage))
@@ -281,8 +322,8 @@ def generate():
   with torch.no_grad():
     for i in range(64):
       z = torch.randn(1, opt.nz, 1, 1, device=device)
-      vutils.save_image(netG(z).detach(
-      ), f"unknown/cifar_fake_{i + 1:04d}.png", normalize=True)
+      fake = netG(z)
+      vutils.save_image(fake.detach(), f"unknown/cifar_fake_{i + 1:04d}.png", normalize=True)
   print("Images have been generated!")
 
 
