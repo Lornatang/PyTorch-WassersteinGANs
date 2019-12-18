b4001de08fce064bcd90475e3c6d4ac473fd364c
Changyu Liu <liuchangyu1111@gmail.com>
2019-12-18 19:43:55 +0800

    Update gan_simpson.py

--- gan_simpson.py
+++ gan_simpson.py
@@ -24,6 +24,10 @@ import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from torch.optim.rmsprop import RMSprop
 
+from wassersteinGANs.model.dcgan import Discriminator
+from wassersteinGANs.model.dcgan import Generator
+from wassersteinGANs.model.dcgan import weights_init
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataroot', type=str, default="./datasets/simpson",
                     help='path to datasets. Must choice it')
@@ -84,98 +88,6 @@ ngf = int(opt.ngf)
 ndf = int(opt.ndf)
 
 
-# custom weights initialization called on netG and netD
-def weights_init(m):
-  classname = m.__class__.__name__
-  if classname.find('Conv') != -1:
-    m.weight.data.normal_(0.0, 0.02)
-  elif classname.find('BatchNorm') != -1:
-    m.weight.data.normal_(1.0, 0.02)
-    m.bias.data.fill_(0)
-
-
-class Generator(nn.Module):
-  def __init__(self, ngpu):
-    super(Generator, self).__init__()
-    self.ngpu = ngpu
-    self.main = nn.Sequential(
-      # inputs is Z, going into a convolution
-      nn.ConvTranspose2d(100, ngf * 8, 4, 1, 0, bias=False),
-      nn.BatchNorm2d(ngf * 8),
-      nn.ReLU(True),
-      # state size. (ngf*8) x 4 x 4
-      nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ngf * 4),
-      nn.ReLU(True),
-      # state size. (ngf*4) x 8 x 8
-      nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ngf * 2),
-      nn.ReLU(True),
-      # state size. (ngf*2) x 16 x 16
-      nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ngf),
-      nn.ReLU(True),
-      # state size. (ngf) x 32 x 32
-      nn.ConvTranspose2d(ngf, 3, 4, 2, 1, bias=False),
-      nn.Tanh()
-      # state size. (nc) x 64 x 64
-    )
-
-  def forward(self, inputs):
-    """ forward layer
-    Args:
-      inputs: input tensor data.
-    Returns:
-      forwarded data.
-    """
-    if torch.cuda.is_available() and self.ngpu > 1:
-      outputs = nn.parallel.data_parallel(
-        self.main, inputs, range(self.ngpu))
-    else:
-      outputs = self.main(inputs)
-    return outputs
-
-
-class Discriminator(nn.Module):
-  def __init__(self, ngpu):
-    super(Discriminator, self).__init__()
-    self.ngpu = ngpu
-    self.main = nn.Sequential(
-      # inputs is (nc) x 64 x 64
-      nn.Conv2d(3, ndf, 4, 2, 1, bias=False),
-      nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf) x 32 x 32
-      nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 2),
-      nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*2) x 16 x 16
-      nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 4),
-      nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*4) x 8 x 8
-      nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(ndf * 8),
-      nn.LeakyReLU(0.2, inplace=True),
-      # state size. (ndf*8) x 4 x 4
-      nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-      nn.Sigmoid()
-    )
-
-  def forward(self, inputs):
-    """ forward layer
-    Args:
-      inputs: input tensor data.
-    Returns:
-      forwarded data.
-    """
-    if torch.cuda.is_available() and self.ngpu > 1:
-      outputs = nn.parallel.data_parallel(
-        self.main, inputs, range(self.ngpu))
-    else:
-      outputs = self.main(inputs)
-    return outputs.view(-1, 1).squeeze(1)
-
-
 def train():
   """ train model
   """
@@ -189,11 +101,15 @@ def train():
   dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size, num_workers=int(opt.workers))
 
   if torch.cuda.device_count() > 1:
-    netG = torch.nn.DataParallel(Generator(opt.ngpu)).to(device)
-    netD = torch.nn.DataParallel(Discriminator(opt.ngpu)).to(device)
+    netG = torch.nn.DataParallel(Generator(ngpu))
+    netD = torch.nn.DataParallel(Discriminator(ngpu))
   else:
-    netG = Generator(opt.ngpu)
-    netD = Discriminator(opt.ngpu)
+    netG = Generator(ngpu)
+    netD = Discriminator(ngpu)
+  netD.apply(weights_init)
+  netG.apply(weights_init)
+  netD.to(device)
+  netG.to(device)
   if opt.netG != "":
     netG.load_state_dict(torch.load(opt.netG, map_location=lambda storage, loc: storage))
   if opt.netD != "":
@@ -282,10 +198,11 @@ def generate():
   ################################################
   print(f"Load model...\n")
   if torch.cuda.device_count() > 1:
-    netG = torch.nn.DataParallel(Generator(opt.ngpu)).to(device)
+    netG = torch.nn.DataParallel(Generator(ngpu=opt.ngpu))
   else:
-    netG = Generator(opt.ngpu)
+    netG = Generator(ngpu=ngpu)
   netG.load_state_dict(torch.load(opt.netG, map_location=lambda storage, loc: storage))
+  netG.to(device)
   with torch.no_grad():
     for i in range(64):
       z = torch.randn(1, opt.nz, 1, 1, device=device)
